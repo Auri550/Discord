4448ac0a14d4a0e531d4eff5568a9fbf0004db43
Glitch (glitch-hello-node) <none>
2022-02-19 07:40:16 +0000

    🕍💕 Checkpoint ./index.js:42394884/1359 ./package.json:42394884/1317

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // Response for Uptime Robot
-const http = require('http')
+const http = require("http");
 http
-  .createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.end('Bot is active \n')
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Bot is active \n");
   })
-  .listen(3000)
+  .listen(3000);
 
 // main
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
 if (process.env.DISCORD_BOT_TOKEN == undefined) {
-  console.error('TOKENが設定されていません。')
-  process.exit(0)
+  console.error("TOKENが設定されていません。");
+  process.exit(0);
 }
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`ログイン: ${client.user.tag}`);
-})
+});
 
-client.on('message', message => {
+client.on("message", (message) => {
   if (message.mentions.users.has(client.user.id)) {
-    message.reply('呼びましたか？')
-    return
+    message.reply("呼びましたか？");
+    return;
   }
-})
+});
 
-client.login(process.env.DISCORD_BOT_TOKEN)
+client.login(process.env.DISCORD_BOT_TOKEN);
